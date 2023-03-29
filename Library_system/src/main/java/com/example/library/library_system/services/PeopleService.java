c029913efceb24d031ee3c03c9e00669e19073e2
levantosina <80888362+Levantosina@users.noreply.github.com>
2023-03-29 17:31:10 +0300

    Add files via upload

---
+++ Library_system/src/main/java/com/example/library/library_system/services/PeopleService.java
@@ -0,0 +1,83 @@
+package com.example.library.library_system.services;
+
+import com.example.library.library_system.models.Book;
+import com.example.library.library_system.models.Person;
+import com.example.library.library_system.repositories.PeopleRepository;
+import org.hibernate.Hibernate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Levantosina
+ */
+@Service
+@Transactional(readOnly = true)
+public class PeopleService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PeopleService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    public List<Person> findAll() {
+        return peopleRepository.findAll();
+    }
+
+    public Person findOne(int id) {
+        Optional<Person> foundPerson = peopleRepository.findById(id);
+        return foundPerson.orElse(null);
+    }
+
+    @Transactional
+    public void save(Person person) {
+        peopleRepository.save(person);
+    }
+
+    @Transactional
+    public void update(int id, Person updatedPerson) {
+        updatedPerson.setId(id);
+        peopleRepository.save(updatedPerson);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        peopleRepository.deleteById(id);
+    }
+
+    public Optional<Person> getPersonByFullName(String fullName) {
+
+        return peopleRepository.findByFullName(fullName);
+    }
+
+    public List<Book> getBooksByPersonId(int id) {
+        Optional<Person> person = peopleRepository.findById(id);
+
+        if (person.isPresent()) {
+            Hibernate.initialize(person.get().getBooks());
+           //We need to initialize the books, because we are going to use them in the next line
+
+
+           //check if the book is expired
+            person.get().getBooks().forEach(book -> {
+                long diffInMillies = Math.abs(book.getTakenAt().getTime() - new Date().getTime());
+                // 864000000 mill.sec = 10 суток
+                if (diffInMillies > 864000000)
+                    book.setExpired(true); // book is expired
+            });
+
+            return person.get().getBooks();
+        }
+        else {
+            return Collections.emptyList();
+        }
+    }
+}
