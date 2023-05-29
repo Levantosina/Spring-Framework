a851d69b4a0dc0ed4cb09d8ffd76c02652895047
levantosina <80888362+Levantosina@users.noreply.github.com>
2023-05-29 17:28:46 +0300

    Delete StationsApp directory

--- StationsApp/Readme.md
+++
@@ -1,2 +0,0 @@
-
-Applications in which the basic methods of obtaining data from a database are implemented.
--- StationsApp/StationsApp/src/main/java/org/example/stations/StationsApp/repositories/StationProjection.java
+++
@@ -1,9 +0,0 @@
-package org.example.stations.StationsApp.repositories;
-
-/**
- * @author Levantosina
- */
-public interface StationProjection {
-    String getDepartureStationName();
-    String getId();
-}
